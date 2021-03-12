433ddd00b5156302953eeca3d90125d9e380cf50
xkovac <xkovaci1@stuba.sk>
2021-03-12 09:47:39 +0100

    feat(mysql):
    initialize project
    
    configure mysql properties

---
+++ src/main/java/sk/sparta/SpartaApplication.java
@@ -0,0 +1,13 @@
+package sk.sparta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/sparta/SpartaApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.sparta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
