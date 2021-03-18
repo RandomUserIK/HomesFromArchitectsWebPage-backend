7f0dc253955560aa4140e95888c490274dbf4733
Lukáš Rezák <rezak@netgrif.com>
2021-03-18 18:09:41 +0100

    HFA-47 - feat(storage): add repository for photo storage

---
+++ src/main/java/sk/hfa/databases/projects/domains/repositories/FileSystemRepository.java
@@ -0,0 +1,32 @@
+package sk.hfa.databases.projects.domains.repositories;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.stereotype.Repository;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
+
+@Repository
+public class FileSystemRepository {
+
+    String RESOURCES_DIR = FileSystemRepository.class.getResource("/").getPath();
+
+    public String save(byte[] content, String imageName) throws Exception {
+        Path newFile = Paths.get(RESOURCES_DIR + new Date().getTime() + "-" + imageName);
+        Files.createDirectories(newFile.getParent());
+        Files.write(newFile, content);
+        return newFile.toAbsolutePath().toString();
+    }
+
+    public FileSystemResource findInFileSystem(String location) {
+        try {
+            return new FileSystemResource(Paths.get(location));
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
+
+
+}
