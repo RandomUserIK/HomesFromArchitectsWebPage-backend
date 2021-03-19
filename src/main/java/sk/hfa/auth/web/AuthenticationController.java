684ceeb45442511c6586461edda936f9ffbadfa9
xkovac <xkovaci1@stuba.sk>
2021-03-19 19:39:21 +0100

    refactor(security-config): cors
    - add cross origin annotation to auth controller

--- src/main/java/sk/hfa/auth/web/AuthenticationController.java
+++ src/main/java/sk/hfa/auth/web/AuthenticationController.java
@@ -3,10 +3,7 @@ package sk.hfa.auth.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sk.hfa.auth.service.interfaces.IAuthenticationService;
 import sk.hfa.auth.web.requestbodies.AuthenticationRequest;
 import sk.hfa.auth.web.responsebodies.AuthenticationResponse;
@@ -16,6 +13,8 @@ import javax.validation.Valid;
 @Slf4j
 @RestController
 @RequestMapping("/api/auth")
+// TODO: remove after CORS policy is configured
+@CrossOrigin("http://localhost:4200")
 public class AuthenticationController {
 
     private final IAuthenticationService authenticationService;
--- src/main/java/sk/hfa/configuration/security/HfaSecurityConfiguration.java
+++ src/main/java/sk/hfa/configuration/security/HfaSecurityConfiguration.java
@@ -15,7 +15,15 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import sk.hfa.configuration.security.jwt.service.interfaces.IJwtService;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Slf4j
 @Configuration
@@ -39,6 +47,24 @@ public class HfaSecurityConfiguration extends WebSecurityConfigurerAdapter {
         this.authenticationEntryPoint = authenticationEntryPoint;
         this.userDetailsServiceImpl = userDetailsServiceImpl;
         this.jwtService = jwtService;
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+        // configuration.addAllowedOrigin("*");
+        // configuration.addAllowedMethod("*");
+        // configuration.addAllowedHeader("*");
+        // configuration.setAllowCredentials(true);
+        // // TODO: remove after CORS policy is properly configured
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 
     @Bean
@@ -66,10 +92,10 @@ public class HfaSecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK))
                 .and()
                 .exceptionHandling()
-                    .authenticationEntryPoint(authenticationEntryPoint)
-                .and()
-                .headers()
-                    .frameOptions().sameOrigin();
+                    .authenticationEntryPoint(authenticationEntryPoint);
+                // .and()
+                // .headers()
+                //     .frameOptions().sameOrigin();
 
         setCsrf(http);
     }
