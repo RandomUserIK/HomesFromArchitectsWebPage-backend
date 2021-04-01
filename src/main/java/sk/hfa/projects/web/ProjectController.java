c761d146ccb337091a5de48f2163753b8a5c095b
Martin Miklovič <miklovic@netgrif.com>
2021-04-01 17:31:41 +0200

    feat(Project controller): Add endpoint for search base on category

--- src/main/java/sk/hfa/projects/web/ProjectController.java
+++ src/main/java/sk/hfa/projects/web/ProjectController.java
@@ -4,10 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sk.hfa.projects.domain.Project;
 import sk.hfa.projects.services.interfaces.IProjectService;
 import sk.hfa.projects.web.domain.responsebodies.MessageResource;
@@ -16,7 +13,7 @@ import sk.hfa.projects.web.domain.responsebodies.ProjectPageMessageResource;
 
 @Slf4j
 @RestController
-@RequestMapping(path = "/api/projects/")
+@RequestMapping(path = "/api/projects")
 public class ProjectController {
 
     private final IProjectService projectService;
@@ -39,8 +36,8 @@ public class ProjectController {
         return ResponseEntity.ok(responseBody);
     }
 
-    @GetMapping(path = "{page}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<MessageResource> getAll(@PathVariable int page) {
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<MessageResource> getAll(@RequestParam("category") String category, @RequestParam("page") int page) {
         Page<Project> result = projectService.getAllOnPage(page);
         MessageResource responseBody = ProjectPageMessageResource.build(result);
         return ResponseEntity.ok(responseBody);
--- src/main/java/sk/hfa/startup/DummyIndividualProjectRunner.java
+++ src/main/java/sk/hfa/startup/DummyIndividualProjectRunner.java
@@ -2,38 +2,25 @@ package sk.hfa.startup;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import sk.hfa.projects.domain.IndividualProject;
+import sk.hfa.projects.domain.Project;
+import sk.hfa.projects.services.interfaces.IProjectService;
 
 @Component
 public class DummyIndividualProjectRunner implements CommandLineRunner {
 
-    // private final IIndividualProjectService projectService;
-    //
-    // public DummyIndividualProjectRunner(IIndividualProjectService projectService) {
-    //     this.projectService = projectService;
-    // }
+    private final IProjectService projectService;
+
+    public DummyIndividualProjectRunner(IProjectService projectService) {
+        this.projectService = projectService;
+    }
 
     @Override
-    public void run(String... args) throws Exception {
-        // for (int i = 0; i < 25; i++) {
-        //     IndividualProject ip = new IndividualProject();
-        //     ip.setProjectName("Project name "+ i);
-        //     ip.setBuiltUpArea(2222.2);
-        //     ip.setPersons(4);
-        //     ip.setGarage(true);
-        //     ip.setRooms(8);
-        //     ip.setEnergeticClass("Test energetic class");
-        //     ip.setOrientation("Test orientation");
-        //     ip.setGrossArea(22.22);
-        //     ip.setSelfHelpBuild(22.222);
-        //     ip.setPriceOnKey(8888.88);
-        //     ip.setBasicProject(7777.77);
-        //     ip.setAdvancedProject(7778.77);
-        //     ip.setTitlePhoto("");
-        //     ip.setFloorPlanPhoto("");
-        //     ip.setAngleOfRoof(60.0);
-        //     ip.setHeatingSource("Test heating source");
-        //     ip.setHeatingType("Test heating type");
-        //     projectService.save(ip);
-        // }
+    public void run(String... args) {
+        for (int i = 0; i < 25; i++) {
+            Project ip = new IndividualProject();
+            ip.setTitle("Test" + i);
+            projectService.save(ip);
+        }
     }
 }
