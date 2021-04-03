dd83865530fc2411254417c7e00ace1134966c70
xkovac <xkovaci1@stuba.sk>
2021-04-03 08:26:31 +0200

    ci(travis):
    - set travis jdk to java 8

--- src/main/java/sk/hfa/startup/AdminRunner.java
+++ src/main/java/sk/hfa/startup/AdminRunner.java
@@ -11,35 +11,35 @@ import sk.hfa.auth.service.interfaces.IRoleService;
 import sk.hfa.auth.service.interfaces.IUserService;
 
 @Slf4j
-// @Component
-public class AdminRunner {
+@Component
+public class AdminRunner implements CommandLineRunner {
 
-    // private final IUserService userService;
-    // private final IRoleService roleService;
-    //
-    // @Value("${hfa.server.admin.username}")
-    // private String username;
-    //
-    // @Value("${hfa.server.admin.password}")
-    // private String password;
-    //
-    // @Autowired
-    // public AdminRunner(IUserService userService, IRoleService roleService) {
-    //     this.userService = userService;
-    //     this.roleService = roleService;
-    // }
-    //
-    // @Override
-    // public void run(String... args) throws Exception {
-    //     log.info("Creating dummy admin user...");
-    //     User adminUser = new User(username, password);
-    //     Role adminRole = new Role("ROLE_ADMIN");
-    //     adminRole = roleService.save(adminRole);
-    //     adminUser.getRoles().add(adminRole);
-    //     adminRole.getUsers().add(adminUser);
-    //     userService.saveUser(adminUser);
-    //     roleService.save(adminRole);
-    //     log.info("Dummy admin user successfully created.");
-    // }
+    private final IUserService userService;
+    private final IRoleService roleService;
+
+    @Value("${hfa.server.admin.username}")
+    private String username;
+
+    @Value("${hfa.server.admin.password}")
+    private String password;
+
+    @Autowired
+    public AdminRunner(IUserService userService, IRoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Creating dummy admin user...");
+        User adminUser = new User(username, password);
+        Role adminRole = new Role("ROLE_ADMIN");
+        adminRole = roleService.save(adminRole);
+        adminUser.getRoles().add(adminRole);
+        adminRole.getUsers().add(adminUser);
+        userService.saveUser(adminUser);
+        roleService.save(adminRole);
+        log.info("Dummy admin user successfully created.");
+    }
 
 }
