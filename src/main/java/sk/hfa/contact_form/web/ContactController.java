76ee8349a9c942e62272d3c6eef7c97ecf85ea63
Martin Miklovič <miklovic@netgrif.com>
2021-04-11 13:26:56 +0200

    feat(Email): Add email client

--- src/main/java/sk/hfa/contact_form/web/ContactController.java
+++ src/main/java/sk/hfa/contact_form/web/ContactController.java
@@ -1,6 +1,7 @@
 package sk.hfa.contact_form.web;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sk.hfa.contact_form.domain.requestbodies.ContactFormRequest;
 import sk.hfa.contact_form.domain.responsebodies.SubmittedContactFormResponse;
+import sk.hfa.email.service.EmailService;
+import sk.hfa.email.service.interfaces.IEmailService;
 import sk.hfa.projects.web.domain.responsebodies.MessageResource;
 import sk.hfa.recaptcha.service.interfaces.IRecaptchaService;
 
@@ -18,13 +21,17 @@ public class ContactController {
 
     private final IRecaptchaService recaptchaService;
 
-    public ContactController(IRecaptchaService recaptchaService) {
+    private final IEmailService emailService;
+
+    public ContactController(IRecaptchaService recaptchaService, IEmailService emailService) {
         this.recaptchaService = recaptchaService;
+        this.emailService = emailService;
     }
 
     @PostMapping("/contact")
     public ResponseEntity<MessageResource> getContactForm(@RequestBody ContactFormRequest contactForm) {
         recaptchaService.processResponse(contactForm.getRecaptchaToken());
+        emailService.send(contactForm);
         MessageResource responseBody = new SubmittedContactFormResponse("success");
         return ResponseEntity.ok(responseBody);
     }
