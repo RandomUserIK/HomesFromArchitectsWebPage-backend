8e86108ed5338be62d2ca784386e77402c769e5f
Martin Miklovič <miklovic@netgrif.com>
2021-04-11 18:32:51 +0200

    feat(Validation email): Add validation constrains to email field in contact form request body

--- src/main/java/sk/hfa/contact_form/domain/requestbodies/ContactFormRequest.java
+++ src/main/java/sk/hfa/contact_form/domain/requestbodies/ContactFormRequest.java
@@ -2,6 +2,7 @@ package sk.hfa.contact_form.domain.requestbodies;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
@@ -16,6 +17,7 @@ public class ContactFormRequest {
     private String surname;
 
     @NotBlank
+    @Email
     private String email;
 
     @NotBlank
