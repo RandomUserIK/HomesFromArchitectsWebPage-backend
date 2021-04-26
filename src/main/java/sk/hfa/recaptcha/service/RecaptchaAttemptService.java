0e23845970c6b9a8332cef3e5a8876c490728100
Martin Miklovič <miklovic@netgrif.com>
2021-04-26 12:02:05 +0200

    feat(Recaptcha): Add service for recording user recaptcha attempts

---
+++ src/main/java/sk/hfa/recaptcha/service/RecaptchaAttemptService.java
@@ -0,0 +1,38 @@
+package sk.hfa.recaptcha.service;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.springframework.stereotype.Service;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class RecaptchaAttemptService {
+
+    private final int MAX_ATTEMPT = 4;
+    private LoadingCache<String, Integer> attemptsCache;
+
+    public RecaptchaAttemptService() {
+        attemptsCache = CacheBuilder.newBuilder()
+                .expireAfterWrite(4, TimeUnit.HOURS).build(new CacheLoader<String, Integer>() {
+                    @Override
+                    public Integer load(String key) {
+                        return 0;
+                    }
+                });
+    }
+
+    public void reCaptchaSucceeded(String key) {
+        attemptsCache.invalidate(key);
+    }
+
+    public void reCaptchaFailed(String key) {
+        int attempts = attemptsCache.getUnchecked(key);
+        attempts++;
+        attemptsCache.put(key, attempts);
+    }
+
+    public boolean isBlocked(String key) {
+        return attemptsCache.getUnchecked(key) >= MAX_ATTEMPT;
+    }
+}
--- src/main/java/sk/hfa/recaptcha/service/RecaptchaService.java
+++ src/main/java/sk/hfa/recaptcha/service/RecaptchaService.java
@@ -1,5 +1,6 @@
 package sk.hfa.recaptcha.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import sk.hfa.recaptcha.domain.GoogleResponse;
@@ -10,17 +11,26 @@ import java.net.URI;
 @Service
 public class RecaptchaService extends AbstractRecaptchaService {
 
+    @Autowired
+    private RecaptchaAttemptService recaptchaAttemptService;
+
     @Override
     public void processResponse(final String response) {
         securityCheck(response);
+
+        if (recaptchaAttemptService.isBlocked(getClientIP()))
+            throw new RecaptchaException("Client is blocked");
+
 
         final URI verifyUri = URI.create(String.format(RECAPTCHA_URL_TEMPLATE, getReCaptchaSecret(), response, getClientIP()));
         try {
             final GoogleResponse googleResponse = restTemplate.getForObject(verifyUri, GoogleResponse.class);
 
             if (googleResponse == null || !googleResponse.isSuccess()) {
+                recaptchaAttemptService.reCaptchaFailed(getClientIP());
                 throw new RecaptchaException("reCaptcha was not successfully validated");
             }
+            recaptchaAttemptService.reCaptchaSucceeded(getClientIP());
         } catch (RestClientException rce) {
             throw new RecaptchaException("Registration unavailable at this time.  Please try again later.");
         }
