aa78c8ac84c33bc1cafd75c8d9f751b6904c61be
Martin Miklovic <martinmiklovic10@gmail.com>
2022-03-26 14:11:19 +0100

    fix(Build): Fix build issue

--- src/main/java/sk/hfa/startup/DummyIndividualProjectRunner.java
+++ src/main/java/sk/hfa/startup/DummyIndividualProjectRunner.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,6 +24,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
+@Profile("dev")
 @Component
 public class DummyIndividualProjectRunner implements CommandLineRunner {
 
