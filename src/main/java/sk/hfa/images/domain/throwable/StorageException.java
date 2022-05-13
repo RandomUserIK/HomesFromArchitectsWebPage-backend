4c3d2e28ea2a338e8612bf0749cd2f13318b0af7
Martin Miklovic <martin.miklovic@engouse.com>
2022-05-13 12:29:28 +0200

    feat(Image service): Fix travis build

---
+++ src/main/java/sk/hfa/images/domain/throwable/StorageException.java
@@ -0,0 +1,13 @@
+package sk.hfa.images.domain.throwable;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
